42466904624ee9da57a052eb4332cdb9399166a3
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-03 18:16:29 -0300

    add post categories, using ClientProxy, ClientProxyFactory with Transport.RMQ

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices'
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private clientAdminBackend: ClientProxy;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:SENHA@IPCONEXAO_RABBITMQ_AWS:PORTA_APLICACAO/VITURAL_HOST'],
+        queue: 'admin-backend'
+      }
+    })
+  }
+
+  @Post('categories')
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.clientAdminBackend.emit('create-category', createCategoryDto);
   }
 }
