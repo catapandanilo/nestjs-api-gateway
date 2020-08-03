0e383e79cd3ae44bffa7c2088aa322d175ca6de6
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-03 17:39:22 -0300

    add AllExceptionsFilter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   Date.prototype.toJSON = function(): any {
     return momentTimezone(this)
