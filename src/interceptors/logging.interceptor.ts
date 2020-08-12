72d742f888f92ab683c3bae02fe04e5ffd87324d
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-12 17:36:19 -0300

    add TimeoutInterceptor using useGlobalInterceptors

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -1,16 +1,11 @@
-import { NestInterceptor, ExecutionContext, CallHandler, Logger } from "@nestjs/common";
+import { NestInterceptor, ExecutionContext, CallHandler, Logger, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
+@Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
     Logger.log('Before...');
-    const now = Date.now();
-
-    return next
-      .handle()
-      .pipe(
-        tap(() => Logger.log(`After...`))
-      );
+    return next.handle().pipe(tap(() => Logger.log(`After...`)));
   }
 }
